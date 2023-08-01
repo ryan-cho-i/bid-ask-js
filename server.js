313fc54b8e6e4b80e3c86130c93d30c86b6b0064
CodingStorageSoo <chlqudtn347@gmail.com>
2023-07-31 23:41:04 -0700

    bid-ask-api

--- server.js
+++ server.js
@@ -1,89 +1,97 @@
-// npm i dotenv
 require("dotenv").config();
-
-// npm init -y
-// npm i express
 const express = require("express");
 const app = express();
 app.use(express.json());
 
-// npm i axios
 const axios = require("axios");
-
-// npm i ioredis
-// redis-server
-// redis-cli
 const Redis = require("ioredis");
 const { json } = require("body-parser");
 const redisClient = new Redis();
 
-app.post("/bidresponse", async (req, res) => {
+async function handleResponse(req, res, orderType) {
   try {
     const { ticker, id, price } = req.body;
-
-    const cnt = await redisClient.incr(`${ticker}-cnt`);
+    const cnt = await redisClient.incr(`${ticker}-${orderType}-cnt`);
+    const orderScore =
+      orderType === "bid" ? price * 10000 - cnt : price * 10000 + cnt;
 
     await redisClient.zadd(
-      `${ticker}-bid`,
-      price * 10000 - cnt,
+      `${ticker}-${orderType}`,
+      orderScore,
       JSON.stringify({ id: id, price: price, order: cnt })
     );
 
     redisClient
       .multi()
-      .zrevrange(`${ticker}-bid`, 0, 0, "WITHSCORES")
-      .zrange(`${ticker}-ask`, 0, 0, "WITHSCORES")
+      .zrevrange(`${ticker}-bid`, 0, -1, "WITHSCORES")
+      .zrange(`${ticker}-ask`, 0, -1, "WITHSCORES")
       .exec(async (err, results) => {
         if (err) {
           console.error("Redis Error:", err);
           return;
         }
 
-        if (results[0][1].length > 0 && results[1][1].length > 0) {
-          const highestBid = JSON.parse(results[0][1][0]);
-          const lowestAsk = JSON.parse(results[1][1][0]);
+        console.log(results[0]);
+        console.log(results[1]);
 
-          const highestBidPrice = parseFloat(highestBid.price);
-          const lowestAskPrice = parseFloat(lowestAsk.price);
-
-          if (highestBidPrice > lowestAskPrice) {
-            try {
-              redisClient
-                .multi()
-                .zrem(`${ticker}-bid`, JSON.stringify(highestBid))
-                .zrem(`${ticker}-ask`, JSON.stringify(lowestAsk))
-                .exec((err) => {
-                  if (err) {
-                    console.error("Error executing transaction:", err);
-                    return res
-                      .status(500)
-                      .json({ error: "Error executing transaction." });
-                  }
-                  return res.json({
-                    highestBid: highestBidPrice,
-                    lowestAsk: lowestAskPrice,
-                  });
-                });
-            } catch (error) {
-              console.error("Error removing highest bid or lowest ask:", error);
-              return res
-                .status(500)
-                .json({ error: "Error removing highest bid or lowest ask." });
-            }
-          }
+        // Check that both bid and ask sets have at least one element
+        if (results[0][1].length == 0 || results[1][1].length == 0) {
+          return res.json({ success: "good" });
         }
-        return res.json({ success: "good" });
+
+        const highestBid = JSON.parse(results[0][1][0]);
+        const lowestAsk = JSON.parse(results[1][1][0]);
+
+        const highestBidPrice = parseFloat(highestBid.price);
+        const lowestAskPrice = parseFloat(lowestAsk.price);
+
+        if (highestBidPrice < lowestAskPrice) {
+          return res.json({ success: "good" });
+        }
+
+        try {
+          redisClient
+            .multi()
+            .zrem(`${ticker}-bid`, JSON.stringify(highestBid))
+            .zrem(`${ticker}-ask`, JSON.stringify(lowestAsk))
+            .exec((err) => {
+              if (err) {
+                console.error("Error executing transaction:", err);
+                return res
+                  .status(500)
+                  .json({ error: "Error executing transaction." });
+              }
+              return res.json({
+                highestBid: highestBidPrice,
+                lowestAsk: lowestAskPrice,
+              });
+            });
+        } catch (error) {
+          console.error("Error removing highest bid or lowest ask:", error);
+          return res
+            .status(500)
+            .json({ error: "Error removing highest bid or lowest ask." });
+        }
       });
   } catch (error) {
-    console.error("Error in /bidresponse endpoint:", error);
-    return res.status(500).json({ error: "Error in /bidresponse endpoint." });
+    console.error(`Error in /${orderType}response endpoint:`, error);
+    return res
+      .status(500)
+      .json({ error: `Error in /${orderType}response endpoint.` });
   }
+}
+
+app.post("/bidresponse", async (req, res) => {
+  handleResponse(req, res, "bid");
 });
 
-// // Get Ask Responses
-// app.post("/askresponse/", async (req, res) => {});
+app.post("/askresponse", async (req, res) => {
+  handleResponse(req, res, "ask");
+});
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
   console.log(`Server Listening on PORT ${PORT}`);
+  await redisClient.flushdb();
+  console.log("Redis database flushed successfully.");
 });
